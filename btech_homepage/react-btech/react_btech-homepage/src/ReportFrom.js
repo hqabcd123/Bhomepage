ec6c0fc51df5d5d5d43f79c7fe531aa1035b33b2
hqabcd123 <hqabcd123@gmail.com>
2022-10-11 17:38:01 +0900

    update

--- btech_homepage/react-btech/react_btech-homepage/src/ReportFrom.js
+++ btech_homepage/react-btech/react_btech-homepage/src/ReportFrom.js
@@ -1,33 +1,47 @@
 import NavScrollExample from './Component/js/Bootstramp';
 import './Component/css/ReportForm.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-function changePosition(){
-    //do something
+function ChangePosition(e){
+    console.log('hello');
+    let x = e.target.offsetLeft;
+    let y = e.target.offsetTop;
+
+    let coords = {
+        x: e.clintX - e.target.offsetLeft,
+        y: e.clintY - e.target.offsetTop,
+    }
+
+    return coords;
 }
 
 
 const Cap = () => {
 
     let [Style, setStyle] = useState({
-        position: 'absolute',
-        right: '100px',
-        top: '25px'
+        position: 'relative',
+        right: '0px',
+        top: '0px'
     })
 
-    const handleClickEvent = () => {
+    const handleClickEvent = (e) => {
         let newStyle = {...Style};
         console.log(newStyle);
-        newStyle['right'] = '500px';
+        let temp = ChangePosition(e);
+        console.log('temp: ' + temp.x);
+        newStyle['right'] = temp.x + 'px';
         setStyle(newStyle)
     };
 
     return(
-        <div style={Style}>
+        <div 
+            style={Style}
+            onMouseDown={(e) => handleClickEvent(e)}
+        >
             <p 
                 className=' text-red-500 text-4xl font-bold text-right'
                 style={Style}
-                onClick={handleClickEvent}
+                // onClick={(e) => handleClickEvent(e)}
             >CONFIDENTIAL</p>
         </div>
     );
@@ -35,6 +49,7 @@ const Cap = () => {
 
 
 function ReportForm() {
+
     return (
         <div>
             <NavScrollExample></NavScrollExample>
